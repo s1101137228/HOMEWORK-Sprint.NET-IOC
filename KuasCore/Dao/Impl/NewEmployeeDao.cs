83b17138d9624315ae6e66130a83633aac44e63b
s1101137228 <k0988721726@yahoo.com.tw>
2015-04-12 13:38:15 +0800

    test

---
+++ KuasCore/Dao/Impl/NewEmployeeDao.cs
@@ -0,0 +1,49 @@
+﻿
+using KuasCore.Dao.Base;
+using KuasCore.Dao.Mapper;
+using KuasCore.Models;
+using Spring.Data.Common;
+using Spring.Data.Generic;
+using System.Collections.Generic;
+using System.Data;
+
+namespace KuasCore.Dao.Impl
+{
+    public class NewEmployeeDao : IEmployeeDao
+    {
+        public IList<Employee> GetAllEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+
+            Employee employee1 = new Employee();
+
+            employee1.Id = "Fisrt";
+            employee1.Name = "悟空";
+            employee1.Age = 65;
+            employees.Add(employee1);
+
+            Employee employee2 = new Employee();
+
+            employee2.Id = "Second";
+            employee2.Name = "悟飯";
+            employee2.Age = 40;
+            employees.Add(employee2);
+
+            return employees;
+        }
+
+        public Employee GetEmployeeById(string id)
+        {
+
+
+            Employee employeeman = new Employee();
+
+            employeeman.Id = "Fisrt";
+            employeeman.Name = "悟空";
+            employeeman.Age = 65;
+
+            return employeeman;
+        }
+
+    }
+}
---
+++ KuasCore/Services/Impl/CompanyService.cs
@@ -0,0 +1,43 @@
+﻿using KuasCore.Models;
+using System.Collections.Generic;
+
+namespace KuasCore.Services.Impl
+{
+    public class CompanyService : ICompanyService
+    {
+
+        public IList<Company> GetAllCompanies()
+        {
+            List<Company> companies = new List<Company>();
+
+            Company company1 = new Company();
+            company1.Id = "s1101137228";
+            company1.Name = "田皓允";
+            companies.Add(company1);
+
+            Company company2 = new Company();
+            company2.Id = "KUAS";
+            company2.Name = "高雄應用科技大學";
+            companies.Add(company2);
+
+            return companies;
+        }
+
+        public Company GetCompanyById(string id)
+        {
+
+            Company company = null;
+            
+            if ("GSS".Equals(id))
+            {
+                company = new Company();
+                company.Id = "s1101137228";
+                company.Name = "田皓允";
+            }
+
+            return company;
+        }
+
+    }
+
+}
